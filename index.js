3c39640ce96b588f495d00f4545fd1248b2adb94
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-11-08 20:58:57 +0600

    basic CRUD setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -12,15 +13,36 @@ app.use(express.json())
 
 
 // console.log(process.env.PORT);
+// console.log(process.env.DB_USERNAME);
+// console.log(process.env.DB_PASSWORD);
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6vknfdj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 const connectDB = async () => {
 
     try {
+        if (client.connect()) {
+            console.log("____________connected____________");
+            const servicesCollection = client.db('ashikurPhotographer').collection('services');
+            const result = await servicesCollection.insertOne({ sercice: "Weeding photography" })
+            console.log(result);
+        }
+
 
     } catch (error) {
-
+        console.log(error);
     }
 }
+
+// collections
+
+
+connectDB();
+
 
 
 
